af1cdb1740fdfa6d95524fdbdccf5d6583fe0811
NadiiaHonchar <17nmg82@gmail.com>
2022-08-19 00:13:52 +0300

    add post

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 const useSocket = require("socket.io");
 
 const app = express();
+app.use(cors());
 const server = require("http").Server(app);
 const io = useSocket(server);
+
+app.use(express.json());
 
 const rooms = new Map();
 
@@ -11,9 +15,23 @@ app.get("/rooms", (req, res) => {
   res.json(rooms);
 });
 
-io.on("connection", (socket)=>{
-    console.log('socket connected', socket.id)
-})
+app.post("/rooms", (req, res) => {
+  const { roomID, userName } = req.body;
+  if (!rooms.has(roomID)) {
+    rooms.set(
+      roomID,
+      new Map([
+        ["users", new Map()],
+        ["messages", []],
+      ])
+    );
+  }
+  res.send();
+});
+
+io.on("connection", (socket) => {
+  console.log("socket connected", socket.id);
+});
 
 server.listen(5000, (err) => {
   if (err) {
