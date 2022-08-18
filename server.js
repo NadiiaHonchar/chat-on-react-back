2be56da4d1d9e293b8b22e5215a4a39340ca1023
NadiiaHonchar <17nmg82@gmail.com>
2022-08-18 22:31:19 +0300

    fixed

--- server.js
+++ server.js
@@ -8,15 +8,14 @@ const io = useSocket(server);
 const rooms = new Map();
 
 app.get("/rooms", (req, res) => {
-//   rooms.set("hello", "");
   res.json(rooms);
 });
 
-io.on("connection", socket=>{
+io.on("connection", (socket)=>{
     console.log('socket connected', socket.id)
 })
 
-app.listen(5000, (err) => {
+server.listen(5000, (err) => {
   if (err) {
     throw Error(err);
   }
